4c63a20acb2a4f9f14556da7d153a1492b5a3b96
maximilianorodriguezpdea-ai <maximilianorodriguezpdea@gmail.com>
2026-08-28 12:48:09 -0400

    2

---
+++ Ejercicio 2/desafio1.py
@@ -0,0 +1,8 @@
+nombre = input("ingrese su nombre:")
+edad = int(input("ingrese su edad:"))
+
+if edad >= 18:
+    print("hola",{nombre}, "usted es mayor de edad")
+else:
+    print("hola,{nombre}, usted es menor de edad")
+    
