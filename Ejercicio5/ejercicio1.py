5b6a81326182b366483bf8aa0cc8677c8a3ee1a6
maximilianorodriguezpdea-ai <maximilianorodriguezpdea@gmail.com>
2026-09-04 12:50:33 -0400

    2

---
+++ Ejercicio5/ejercicio1.py
@@ -0,0 +1,9 @@
+num = int(input("ingrese un numero q multiplicara:"))
+inicio = 1
+
+while inicio <= 10:
+    resultado = num * inicio
+    print(f"{num} x {inicio} = {resultado}")
+    inicio = inicio + 1
+print("fin del programa")
+
